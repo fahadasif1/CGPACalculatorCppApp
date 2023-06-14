136aacd7f89553fd55dbca8e8ef599faa0d15440
fahadsmac <chaudh43@uni.coventry.ac.uk>
2023-06-14 09:52:47 +0100

    Initial Commit

---
+++ CGPACalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CGPACalculator
+//
+//  Created by Fahad Asif Chaudhry on 14/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
