82d1ae1bebfa1f98171e442ae76c5694e9fdd452
fahadsmac <fahad.asif1@yahoo.com>
2023-06-14 10:49:35 +0100

    welcome set up and functions

--- CGPACalculator/main.cpp
+++ CGPACalculator/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+// using directive
+using namespace std;
+
+void welcome() {
+    cout << "Welcome to CGPA Calculator!\n";
+    cout << "To calculate your CGPA enter the subject name followed by a space then your marks.\n";}
+
+void inputData(){
+    
+}
+
+void marksToGradePointTable(){
+    
+}
+
+void cgpaCalculator(){
+    
+    
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    welcome();
     return 0;
 }
+
